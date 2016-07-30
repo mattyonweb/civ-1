8c5578eae34c6deb31f8ee8d396686cb78f88744
mattyonweb <mattycaos@gmail.com>
2016-07-30 02:52:04 +0000

    docs

--- world.py
+++ world.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
-import noise, random, engine
-height = 600
-width = 300
+import noise, random, engine, math
+height = 400
+width = 750
 
 class World():
 
@@ -10,12 +10,15 @@ class World():
 		self.height = y
 		#attenzione: alla heightmap bisogna accedere così:
 		#self.heightmap[y][x] e non il contrario!!!
-		self.heightmap = self.create_heightmap(8, 0.8, 2.6)
-		self.tempmap = []
-		self.civs = {}
-		self.img = self.create_image()
+		self.heightmap = self.create_heightmap(8, 0.8, 2.6) #matrix altezze
+		self.tempmap = self.create_tempmap() #la matrix delle temperature
+		self.civs = {} #dizionario civ_obj:(x,y)
+		self.img = self.create_image() #immagine del mondo
 
+
+	# --- GENERATION ---
 	def create_heightmap(self, octave=2, persistence=0.5, lacunarity=2.0):
+		''' Ritorna la heightmap matrix '''
 		random_start = random.random()*1134.72864
 		l = []
 		for y in range(height):
@@ -27,10 +30,31 @@ class World():
 			l.append(l1)
 		return l
 
-	def create_tempmap(
+	def create_tempmap(self, octave=7, persistence=0.9, lacunarity=2.1):
+		''' Ritorna la tempmap matrix '''
+		random_start = random.random()*1134.72864
+		l = []
+		for y in range(height):
+			l1 = []
+			for x in range(width):
+				#val = self.bell_curve(y, 350, height/2, (50*10**-3))
+				val = abs(int(noise.snoise2(random_start + x/1751, random_start + y/1571,
+											octave, persistence, lacunarity)*350))
+				h = self.heightmap[y][x]
+				if h > 90:
+					delta = h * 4/165 + 1
+				else:
+					delta = 0
+				l1.append(val-delta)
+			l.append(l1)
+		return l
 
+
+	# --- PAINTING ---
 	def create_image(self):
-		img = Image.new( 'RGB', (self.width, self.height), "black") # create a new black image
+		''' Ritorna immagine del territorio (senza civiltà, quelle vanno aggiunte
+		direttamente a self.img) '''
+		img = Image.new( 'RGB', (self.width, self.height), "black")
 		pixels = img.load()
 
 		#0-60: blu mare
@@ -53,40 +77,73 @@ class World():
 					c = (255,255,255)
 				pixels[x,y] = c #le coordinate boh
 		return img
+
+	def tempmap_image(self):
+		''' Ritorna l'immagine delle temperature; bianco è più caldo, e vic. '''
+		img = Image.new( 'RGB', (self.width, self.height), "black")
+		pixels = img.load()
+
+		for y in range(img.size[1]):
+			for x in range(img.size[0]):
+				val = self.tempmap[y][x]
+				pixels[x,y] = (int(val*350/255), int(val*350/255), int(val*350/255))
+		return img
+
+	def blend_height_temp(self):
+		''' Ritorna l'immagine miscuglio tra temperatura e territorio '''
+		return Image.blend(self.img, self.tempmap_image(), 0.55)
+
+	def save_images(self):
+		''' Salva tutte le immagini del mondo '''
+		self.img.save("./output/terrain.jpg")
+		self.tempmap_image().save("temperature.jpg")
+		self.blend_height_temp().save("blend.jpg")
 		
 	def show_image(self):
-		#self.create_image().show()
+		''' Mostra l'immagine del territorio salvata '''
 		self.img.show()
-		for civ in self.civs:
-			print(civ.nome, self.civs[civ])
+
+	# --- CIV INTERFACES ---
 		
-	def add_new_civ(self, civ_obj):
-		self.civs[civ_obj] = civ_obj.x0, civ_obj.y0
-		self.mark_civ_on_map(civ_obj)
-		
+	def add_new_civ(self, civ):
+		''' Procedura per aggiungere civiltà al mondo.
+		Prima le si aggiunge al dizionario delle civiltà;
+		poi le si marca sulla cartina del territorio '''
+		civ.x, civ.y = self.place_civ()
+		self.civs[civ] = civ.x, civ.y
+		self.mark_civ_on_map(civ)
+
+	def place_civ(self):
+		''' Ritorna una locazione non marina per l'insediamento della civ. '''
+		while True:
+			x, y = random.randint(0, width-1), random.randint(0, height-1)
+			if self.heightmap[y][x] >=55:
+				return x, y
+				
 	def mark_civ_on_map(self, civ_obj, r=10):
-		x = civ_obj.x0
-		y = civ_obj.y0
+		''' Segnala su self.img la posizione delle cività. Da invocare ogni volta
+		che self.img viene per qualche motivo ricreata! '''
+		x = civ_obj.x
+		y = civ_obj.y
 		name = civ_obj.nome
 		draw = ImageDraw.Draw(self.img)
 		draw.ellipse((x-r, y-r, x+r, y+r), fill = 'red', outline ='white')
 		draw.ellipse((x-r/5, y-r/5, x+r/5, y+r/5), fill = 'white', outline ='white')
 
 		font = ImageFont.truetype("Ticketing.ttf",15)
-		draw.text((x+r,y+r),name + " " + str(x) + "," + str(y),(0,0,0), font=font)
+		draw.text((x,y+r), name + " " + str(x) + "," + str(y), (0,0,0), font=font)
+
+	# --- UTILS ---
+	def bell_curve(self, x, a, b, c):
+		return a*math.e**((-(x-b)**2)/2*c**2)
+		
 #------------------------------
 
 class Civilta():
 	def __init__(self, world, nome):
 		self.nome = nome
-		self.x0, self.y0 = self.place_civilta(world)
+		self.x, self.y = 0, 0
 		world.add_new_civ(self)
-
-	def place_civilta(self, world):
-		while True:
-			x, y = random.randint(0, width-1), random.randint(0,height-1)
-			if world.heightmap[y][x] >=55: #forse è il contrario
-				return x, y
 
 #----------------------------------
 
@@ -95,4 +152,5 @@ lang = engine.Language()
 for _ in range(15):
 	civ = Civilta(world, lang.generate_word())
 
+world.save_images()
 world.show_image()
